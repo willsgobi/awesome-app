7692c03a023581697434944d4c89663c89cab243
William Sgobi <william_sgobi@hotmail.com>
2023-11-26 00:58:17 -0300

    feat: criação de um arquivo para organizar as cores que será utilizadas no decorrer do projeto

---
+++ lib/src/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color darkBlue = Color.fromRGBO(27, 77, 101, 1);
+const Color aqua = Color.fromRGBO(41, 151, 143, 1);
+const Color green = Color.fromRGBO(68, 189, 110, 1);
