66086e402eb689473a3fdce8e110b67af50f5f68
William Sgobi <william_sgobi@hotmail.com>
2023-11-29 07:51:57 -0300

    feat: implementação de testes de widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,21 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:awesome_app/src/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:awesome_app/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('test in LoginPage', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: LoginPage()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Usuário'), findsOneWidget);
+    expect(find.text("Senha"), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    expect(find.text("Política de Privacidade"), findsOneWidget);
+    expect(find.widgetWithText(ElevatedButton, "Entrar"), findsOne);
+
+    await tester.tap(find.widgetWithText(ElevatedButton, "Entrar"));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text("O campo deve ser preenchido!"), findsNothing);
   });
 }
